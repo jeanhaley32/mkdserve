37278e5318dff69130c78963f1e2fd7b927dcaf6
Jean Haley <jean@haley.nyc>
2023-08-03 23:48:42 +0000

    Added flag for "connectionLimit" to set a limit for simultaneous connections Cleaned up variable definitions, defined all strings on one line. w

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -10,24 +11,22 @@ import (
 	"golang.org/x/sync/semaphore"
 )
 
-const ConnectionLimit = 100
-
 var (
-	port   = ""
-	ip     = ""
-	socket = ""
-	page   = ""
+	port, ip, socket, page string
+	ConnectionLimit        int64
 )
 
 func init() {
 	flag.StringVar(&ip, "ip", "127.0.0.1", "IP address to listen on, defaults to 127.0.0.1 if not set")
 	flag.StringVar(&port, "port", "8080", "Port to listen on, defaults to 8080 if not set")
 	flag.StringVar(&page, "main", "main.md", "Main markdown file to serve:, defaults to main.md if not set")
+	flag.Int64Var(&ConnectionLimit, "limit", 100, "Connection limit, defaults to 100 if not set")
 	flag.Parse()
 	socket = ip + ":" + port
 }
 
 func main() {
+	fmt.Println(ConnectionLimit)
 	sem := semaphore.NewWeighted(ConnectionLimit)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
