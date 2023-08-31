6620dea60528bae8c4f5b46507b786a7a377e9a3
Jean Haley <jean@haley.nyc>
2023-08-31 03:11:43 +0000

    enabling Strict Transport Security headers if TLS is enabled

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 var (
 	ip, socket, page, key, csr string
 	ConnectionLimit            int64
+	TLS                        bool
 )
 
 func init() {
@@ -20,9 +21,20 @@ func init() {
 }
 
 func main() {
+	if csr != "" && key != "" {
+		TLS = true
+		socket = ip + ":443"
+	} else {
+		TLS = false
+		socket = ip + ":80"
+	}
 	// Handle main page
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// If root path, serve main page
+		if TLS {
+			w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
+			r.URL.Scheme = "https"
+		}
 		if r.URL.Path == "/" {
 			http.ServeFile(w, r, page)
 		}
@@ -30,23 +42,34 @@ func main() {
 		target := r.URL.Path[0:]
 		http.ServeFile(w, r, "./pages/"+target+".html")
 	})
-	// Handle Image subdirectory
-	http.Handle("/image/", http.StripPrefix("/image/", http.FileServer(http.Dir("image"))))
+
+	// Handle image page
+	http.HandleFunc("/image/", func(w http.ResponseWriter, r *http.Request) {
+		if TLS {
+			w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
+			r.URL.Scheme = "https"
+		}
+		http.ServeFile(w, r, "index.html")
+	})
 
 	// Handle Assets subdirectory
-	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	http.HandleFunc("/assets/", func(w http.ResponseWriter, r *http.Request) {
+		if TLS {
+			w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
+			r.URL.Scheme = "https"
+		}
+		http.ServeFile(w, r, r.URL.Path[1:])
+	})
 
 	log.Printf("Starting server on http://%s\n", socket)
 
 	// If SSL certificate and key are provided, start server with TLS
-	if csr != "" && key != "" {
-		socket := ip + ":443"
+	if TLS {
 		log.Printf("Starting Encrtyped TLS server on https://%s\n", socket)
 		if err := http.ListenAndServeTLS(socket, csr, key, nil); err != nil {
 			log.Fatal(err)
 		}
 	} else {
-		socket := ip + ":80"
 		// If SSL certificate and key are not provided, start server without TLS
 		log.Printf("Starting http server on http://%s\n", socket)
 		if err := http.ListenAndServe(socket, nil); err != nil {
